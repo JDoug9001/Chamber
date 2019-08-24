eb4eb332702494054d60ea6f0fe24d7ed534c529
JDoug9001 <J.Douglas1911@gmail.com>
2019-08-24 14:45:28 -0400

    change to .ino to activate arduino software

---
+++ Mk1_0.ino
@@ -0,0 +1,193 @@
+#include "avr/interrupt.h" 
+
+volatile int value=0;
+const byte Solenoid = 13;
+const byte IntPin = 2;
+volatile byte state = 0;
+
+//Testing 1 2 3
+
+void setup() {
+
+{
+  pinMode (solenoid, OUTPUT);
+  pinMode (IntPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(IntPin), blink, RISING);
+
+// Variables 
+
+int ProxCounter = 0; // counter for the number of Prox Sensor        
+int LastProxState = 0; // previous state of the Prox Sensor
+int Reload = 0; //Initiate Reload State 
+int RandomInt = 0; //Random number for malfunction
+int PrevRandInt = 0; //Previous random number to prevent repeated value
+int Malfunction = 0; //Malfuntion function  
+int SwitchFunctionButton_State = 1; //Button State for switching functions
+int Timeout = 0; //
+
+
+// Inputs and Outputs
+
+pinMode (ProxState, INPUT_PULLUP); //current state of the Prox Sensor
+pinMode (MagButton, INPUT); //Momentary push for mag "tap"
+pinMode (SlideTof, INPUT); //SlideTOF sensor for distance shows slide distance from device
+pinMode (MagTof_OldValue, INPUT); //Old value for MagTof
+pinMode (Solenoid, OUTPUT); //Solenoid to push the slide
+pinMode (SwitchFunctionButton, INPUT); //Button to switch between the three functions
+pinMode (Blue_Led_1, OUTPUT); //LED to indicate which function is being run
+pinMode (Blue_Led_2, OUTPUT); //LED to indicate which function is being run
+pinMode (Blue_Led_3, OUTPUT); //LED to indicate which function is being run
+pinMode (RGB_Led, OUTPUT); //LED to indicate power level 
+pinMode (VibrationSens, INPUT_PULLUP); //Motion switch to turn on 
+}
+}
+
+
+//Deep sleep
+// Interrupt for turning on device goes here set Timeout = 1. 
+// If motion sensor isnt hit again within X time limit. timeout resets to zero. Program exits the loop
+
+ 
+
+
+void loop() {
+
+{
+
+
+    if((value==1)&&(Malfunction !=1)&&(Reload !=1)
+    {
+      digitalWrite(Solenoid, HIGH);
+      delay(500);
+      digitalWrite(Solenoid, LOW);
+      delay(500);
+
+      value=0;
+    }
+}
+
+      void blink(){
+        value = 1;
+        sei();
+      }
+
+
+
+
+
+
+  //Button to switch between programs
+    SwitchFunctionButton_State = digitalRead(Button Pin);
+
+    if(SwitchFunctionButton_State == HIGH){
+        SwitchFunctionButton_State++
+    
+    if(SwitchFunctionButton_State>3){
+       SwitchFunctionButton_State = 1;
+    }
+    }
+
+
+  //Continuous Function Called
+    if (SwitchFunctionButton==1){
+  Blue_Led_1 = HIGH;
+    }
+ //End Continuous Function
+
+
+
+
+
+//Reload Function Called
+  if ((SwitchFunctionButton==2)&&(Reload!=1)){
+
+Blue_Led_1 = HIGH;
+Blue_Led_2 = HIGH;
+
+// Detect prox sensor going high and count number of occurences 
+  if (ProxState = RISING) {  
+      ProxCounter++;
+  }
+// interrupt main interrupt; //stops cycling.....Nest an if loop into interrupt function if("Reload!=1")
+  if (ProxCounter == 15){
+      Reload = 1;
+      } 
+
+  if (Reload == 1){ //Tof Sensor on top of Mag compares distance value. When the distance increases (the mag has been dropped) the loop finds a new number, ProxCounter resets. When the distance decreases (the mag has been reinserted) the reload is reset to zero. 
+      int MagTof_NewValue = analogRead(A?); //Read the input on analog pin
+          if(MagTof_NewValue < MagTof_OldValue){ //The Zero value for Old value can be changed in the future to the value equal to the sensor home distant from the bottom of the slide
+              RandomInt = PrevRandInt; //Find a new random number ^look up at random function^
+              ProxCounter = 0; //Reset the count to zero 
+                  if(MagTof_NewValue > MagTof_OldValue){ 
+                      Reload=0; //Reset Main interrupt           
+                      }
+                      }
+                      }
+//End of Reload Program  
+  }
+
+
+    
+//Random Function Called
+if ((SwitchFunctionButton==3)&&(Reload!=1)){{
+
+Blue_Led_1 = HIGH;
+Blue_Led_2 = HIGH;
+Blue_Led_3 = HIGH;
+
+      
+      //Generate a random number. If it's that same as the previous number, generate a new number
+       RandomInt = random(16); //Random number between 0-15
+          while(RandomInt == PrevRandInt){
+          RandomInt = random(16); //Random number between 0-15
+          }
+  
+    // Detect prox sensor going high and count number of occurences 
+        if (ProxState = RISING){  
+            ProxCounter++;
+            }  
+
+    //Stop main interrupt from functioning when the random number and counter number are the same
+       if (ProxCounter == RandomInt){
+            Malfunction = 1;    //In main interrupts Malfunction!=1
+            }
+
+    //If Momentary is pushed then Tof reaches certain distance go high then malfunction is reset to zero and then the cycle will continue to loop until 15 actuations are reached
+       if (Malfunction = 1){
+         //Have an interrupt to activate sensors
+           digitalRead (MagButton); //"Mag" is tapped
+            delay 500 //Time between tapping mag and pulling slide
+                if(MagButton == HIGH){
+                 analogRead (SlideTof);
+                }
+                }
+                 if (SlideTof> XXX){  //Tof sensor says slide was pulled back X distance ie racked. Solenoid is turned off 
+                   Malfunction = 0;     //Malfunction is cleared
+                   }
+                                 
+    // interrupt main interrupt; //stops cycling.....Nest an if loop into interrupt function if("Reload!=1")
+      if (ProxCounter == 15){
+          Reload = 1;
+          } 
+          }
+     if (Reload == 1){ //Tof Sensor on top of Mag compares distance value. When the distance increases (the mag has been dropped) the loop finds a new number, ProxCounter resets. When the distance decreases (the mag has been reinserted) the reload is reset to zero. 
+          analogRead (MagTof_NewValue); //Read the input on analog pin
+             if(MagTof_NewValue < MagTof_OldValue){ //The Zero value for Old value can be changed in the future to the value equal to the sensor home distant from the bottom of the slide
+                  RandomInt = PrevRandInt; //Find a new random number ^look up at random function^
+                  ProxCounter = 0; //Reset the count to zero 
+                   Mag_Removed = 1; 
+             }
+                     if(Mag_Removed = 1){ 
+                      analogRead (MagTof_NewValue); //Read the input on analog pin
+                     }
+                     }
+                     if(MagTof_NewValue > MagTof_OldValue){
+                        Reload=0; //Reset Main interrupt           
+                         }
+                         }
+//End of Random Malfunction Program 
+}
+
+  
+    
+   
