11a8232dbde064d6e5e65229c4ff4a801f220213
Aaron Watkins <ac.watkins80@gmail.com>
2019-10-29 21:04:16 -0400

    made the transmitter interrupt driven

--- Rx.ino
+++ Rx.ino
@@ -2,15 +2,12 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
-
-
 RF24 radio(7, 8); // CE, CSN
 const byte addresses[][6] = {"00001", "00002"};
 
-
 void setup() {
   Serial.begin(9600);
-    radio.begin();
+  radio.begin();
   radio.openReadingPipe(1, addresses[1]); // 00002
   radio.setPALevel(RF24_PA_MIN);
 }
@@ -18,12 +15,12 @@ void setup() {
 void loop() {
   delay(5);
   radio.stopListening();
-    delay(5);
+  delay(5);
   radio.startListening();
  if (radio.available()) {
     char text[32] = "";
     radio.read(&text, sizeof(text));
     Serial.println(text);
-    delay(250);
+    delay(1000);
   }
 }
--- Tx.ino
+++ Tx.ino
@@ -1,29 +1,28 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include "avr/interrupt.h" 
 
 #define button 4
 
 RF24 radio(7, 8); // CE, CSN
 const byte addresses[][6] = {"00001", "00002"};
-boolean buttonState = 0;
+const byte IntPin1 = 4;
 
 void setup() {
-  pinMode(button, INPUT);
+  pinMode (IntPin1, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(IntPin1), MagTap_ISR, RISING);
   radio.begin();
   radio.openWritingPipe(addresses[1]); // 00002
   radio.setPALevel(RF24_PA_MIN);
 }
 
-void loop() {
-  delay(5);
-   char text[8]= "" ;
-   buttonState = digitalRead(button);
-  if (buttonState == HIGH) {
-    strcpy(text, "Active");
-   }else{
-  strcpy(text, "Dormant");
-    
-    }
-  radio.write(&text,sizeof(text));
-  }
+
+void loop() {}
+
+
+void MagTap_ISR(){
+  char text[] = "Button High!";
+  radio.write(&text, sizeof(text));
+  sei();
+}
