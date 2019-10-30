2eac69030fb6aed653ffe5a102dde22a9b5bb23b
JDoug9001 <J.Douglas1911@gmail.com>
2019-10-29 20:52:40 -0400

    Working Transmission Code

--- Tx.ino
+++ Tx.ino
@@ -2,31 +2,28 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
-#define led 12
+#define button 4
 
 RF24 radio(7, 8); // CE, CSN
 const byte addresses[][6] = {"00001", "00002"};
 boolean buttonState = 0;
 
 void setup() {
-  pinMode(12, OUTPUT);
+  pinMode(button, INPUT);
   radio.begin();
   radio.openWritingPipe(addresses[1]); // 00002
-  radio.openReadingPipe(1, addresses[0]); // 00001
   radio.setPALevel(RF24_PA_MIN);
 }
 
 void loop() {
   delay(5);
-  radio.startListening();
-  radio.read(&buttonState, sizeof(buttonState));
+   char text[8]= "" ;
+   buttonState = digitalRead(button);
   if (buttonState == HIGH) {
-  radio.stopListening();
-   const char text[] = "Active";
-//  radio.write(text);
-  }else{
-    const char text[] = "Dormant";
-    radio.write(text);
+    strcpy(text, "Active");
+   }else{
+  strcpy(text, "Dormant");
+    
     }
-  
+  radio.write(&text,sizeof(text));
   }
